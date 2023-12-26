b320d07f6666cf018b6ec6e07ce52b9a2fd2b78d
Leon <75062991+LiuXiaoyang1111@users.noreply.github.com>
2023-12-26 16:10:31 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -139,6 +139,7 @@ else:
 if AGE != None and R3 != None and GENDER != None and PTHOME != None and DXSUM1 != None and MONTH != None and GDDROP != None and Q13SCORE != None and MMYEAR != None and MMDAY != None and MMHOSPIT != None and MMFLOOR != None and MMTREE != None and MMBALLDL != None and MMTREEDL != None and FAQFORM != None and FAQTRAVL != None and NPIG != None:
     st.subheader("结果输出🖨️")
     prob = INTERCEPT_coef+AGE_coef * AGE + R3_coef * R3 + GENDER_coef * GENDER + PTHOME_coef * PTHOME + DXSUM1_coef * DXSUM1 + MONTH_coef * MONTH + DXSUM1_MONTH + GDDROP_coef * GDDROP + Q13SCORE_coef * Q13SCORE + MMYEAR_coef * MMYEAR + MMDAY_coef * MMDAY + MMHOSPIT_coef * MMHOSPIT + MMFLOOR_coef * MMFLOOR + MMTREE_coef * MMTREE + MMBALLDL_coef * MMBALLDL + MMTREEDL_coef * MMTREEDL + FAQFORM_coef * FAQFORM + FAQTRAVL_coef * FAQTRAVL + NPIG_coef * NPIG
+    st.write(prob)
     if prob < 0 or prob > 1:
         st.write("无法预测认知程度的恶化概率")
     else:
